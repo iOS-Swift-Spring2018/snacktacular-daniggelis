ffeaf76b71bde365b964e73bc7fc13e627bad4c6
Tiffany <tiffany@tiffanys-macbook-air.local>
2018-04-19 01:22:53 -0400

    Initial Commit

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,36 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Tiffany on 4/11/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Spots{
+    var spotArray = [Spot]()
+    var db: Firestore!
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    
+    func loadData(completed: @escaping () -> ()) {
+        db.collection("spots").addSnapshotListener{(querySnapshot, error) in
+            guard error == nil else {
+                return completed()
+            }
+            self.spotArray = []
+            
+            for document in querySnapshot!.documents{
+                let spot = Spot(dictionary: document.data())
+                spot.documentID = document.documentID
+                self.spotArray.append(spot)
+            }
+            completed()
+        }
+    }
+}
+
